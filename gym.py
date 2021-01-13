e17c7690deb56f0021ca2a6a03fecf82e5e12998
Gabriella Szigeti <gbszgtgit config --global user.name Gabriella>
2021-01-13 16:15:05 +0100

    deleted comments, added grid to the board

--- gym.py
+++ gym.py
@@ -1,4 +1,3 @@
-
 from string import ascii_uppercase
 
 
@@ -8,27 +7,29 @@ board_size = 5
 
 def init_board(board_size):
     for i in range(0, board_size):
-        board.append(["0 "] * (board_size))
+        board.append(["0"] * (board_size))
     return board
 
 
 def header(board):
-    print("🚢 ".rjust(2), end="")
+    print("\033[0;34;48m     Battleship Game  \n")
+    print("🚢".ljust(2), end=" ")
     for i in range(1, board_size + 1):
-        print(f"{i}" .rjust(2), end=" ")
+        print(f"{i}" .ljust(3), end=" ")
     print("\n")
-    
+
 
 def print_board(board, board_size):
     for i in range(0, board_size):
-        row = board[i]
+        row = "|".join(board[i])
         print(f"{ascii_uppercase[i].center(3)}", end=" " + " ".join(row))
-        #print("-+-".join(row))
         print("\n")
-
 
 
 
 init_board(5)
 header(board)
+board[0][3] = "H"
+board[2][3] = "M"
+board[3][1] = "B"
 print_board(board,board_size)
