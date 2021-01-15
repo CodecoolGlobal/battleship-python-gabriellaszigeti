2b8d07a989d5c60fe0b92d02d6b2e1c2d9c15099
kprohaszka <cc.kprohaszka@gmail.com>
2021-01-15 02:28:51 -0800

    Create get_player_name(): function.
    The function asks for the player's name.

--- battleship.py
+++ battleship.py
@@ -14,3 +14,12 @@ from battleship_ascii import battleship_rules
 def clear():
     '''clears console'''
     os.system("clear")
+
+
+def get_player_name(numero):
+    '''Gets player name takes NO. parameter'''
+    clear()
+    print(battleship_art[8])
+    player_name = input(f"Please enter Player{numero} name: \n")
+    clear()
+    return player_name
