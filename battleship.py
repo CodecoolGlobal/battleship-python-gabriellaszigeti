6ba78838b6dac9bf8f290af4ad825c3b306dc08a
kprohaszka <cc.kprohaszka@gmail.com>
2021-01-15 02:33:38 -0800

    Create ask_for_user_input(): function.
    Asks for user input, returns it in a tuple format. It checks for errors.

--- battleship.py
+++ battleship.py
@@ -44,3 +44,19 @@ def init_board(board_size):
     for i in range(0, board_size):
         board.append(["0"] * (board_size))
     return board
+
+
+def ask_for_user_input():
+    '''Asks for input checks if it"s on the table '''
+    while True:
+        try:
+            user_input = input('Please enter the coordinates: ')
+            if user_input[0].isalpha() == True and user_input[1].isdigit() == True and (user_input[1] != '0') == True:
+                break
+            else:
+                print("You have entered invalid coordinates, please correct them.")
+        except(IndexError, ValueError):
+            print("You have entered invalid coordinates, please correct them.")
+    user_input_in_touple = (
+        ord(user_input.lower()[0]) - ord('a'), int(user_input[1:])-1)
+    return user_input_in_touple
