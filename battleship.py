03d5c86e7d0b67c16b80178f1ab99acb331b7063
kprohaszka <cc.kprohaszka@gmail.com>
2021-01-15 02:30:28 -0800

    Create game_mode(): function.
    This practically functions as a menu, player can choose Single or Multi.

--- battleship.py
+++ battleship.py
@@ -23,3 +23,16 @@ def get_player_name(numero):
     player_name = input(f"Please enter Player{numero} name: \n")
     clear()
     return player_name
+
+
+def game_mode():
+    '''returns single player or multiplayer option'''
+    clear()
+    print(battleship_art[14])
+    while True:
+        print("Press 1 to Single Player\n\n")
+        print("Press 2 to Multiplayer ")
+        user_input = input("")
+        if user_input == "1" or user_input == "2":
+            clear()
+            return user_input
